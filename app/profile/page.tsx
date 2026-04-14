1cc15d64c9b069dbb85fc29130361775ace6f3fd
JotaBarbosaDev <jotambbarbosa@gmail.com>
2026-04-14 10:18:50 +0100

    Pepare next steps, commented in profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -90,7 +90,7 @@ export default function Table1() {
 
                 {/* Aqui vão as tabs para as secções do perfil (pagamentos - medalhas - Kits(5)) */}
                 {/* Aqui vai o resultado do render da tab acima */}
-                
+                 
                 <div className="w-full h-auto p-10">
                   <ModalEditProfile />
                 </div>
